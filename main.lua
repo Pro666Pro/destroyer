13abbe2377091a1c5bdf0edf05d9be5595ea86b6
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-10 10:04:11 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -62,5 +62,7 @@ game.ReplicatedStorage.Ghostinvisibilityactivated:FireServer()
 wait(0.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-810.1619873046875, 328.2247009277344, 3.9900460243225098)
 wait(0.5)
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+wait(0.5)
 --Made By Kosovo#3784
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Yousuck780/fling-script-V2-Last-version/main/Added%20r15%20support!!!!"), true))()
