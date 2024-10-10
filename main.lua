618834d3623e4a8d4f11d8a0e443f44536b093f0
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-10 10:08:28 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -60,9 +60,9 @@ fireclickdetector(workspace.Lobby["Ghost"].ClickDetector)
 wait(0.5)
 game.ReplicatedStorage.Ghostinvisibilityactivated:FireServer()
 wait(0.5)
+fling = true
+while fling == true do
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-810.1619873046875, 328.2247009277344, 3.9900460243225098)
-wait(0.5)
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-wait(0.5)
+end
 --Made By Kosovo#3784
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Yousuck780/fling-script-V2-Last-version/main/Added%20r15%20support!!!!"), true))()
