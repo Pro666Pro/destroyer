2acf1dd5b41c6838b34f4ab79ccf59963051465b
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-19 22:53:06 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,39 +7,9 @@ wait(0.5)
 
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Whoever made this script, thanks" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "Ok"})
 
-local Noclip = nil
-local Clip = nil
-
-function noclip()
-	Clip = false
-	local function Nocl()
-		if Clip == false and game.Players.LocalPlayer.Character ~= nil then
-			for _,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-				if v:IsA('BasePart') and v.CanCollide and v.Name ~= floatName then
-					v.CanCollide = false
-				end
-			end
-		end
-		wait(0.21) -- basic optimization
-	end
-	Noclip = game:GetService('RunService').Stepped:Connect(Nocl)
-end
-
-function clip()
-	if Noclip then Noclip:Disconnect() end
-	Clip = true
-end
-
-noclip() -- to toggle noclip() and clip()
-
 wait(0.5)
 
-local tp = workspace.Lobby.Teleport1
-local tpp = workspace.Lobby.Teleport2
-tp:Destroy()
-tpp:Destroy()
-wait(0.1)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(273.7153015136719, 357.92523193359375, 33.86956024169922)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(286.3288269042969, 357.1037292480469, 34.369651794433594)
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Script By Nexer" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Credits",Text = "Fling Script By Kosovo" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 wait(0.5)
@@ -48,6 +18,6 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/Yousuck780/fling-scr
 fling = true
 wait(0.5)
 while fling == true do
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(273.7153015136719, 357.92523193359375, 33.86956024169922)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(286.3288269042969, 357.1037292480469, 34.369651794433594)
 wait(0.1)
 end
